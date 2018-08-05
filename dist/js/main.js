68a744339a4bc779e42f83bfde4986271a3f4508
viclotana <viclotana@gmail.com>
2018-08-05 19:43:24 +0100

    ups

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+// select dom elements
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.btn-nav');
+const menuBranding = document.querySelector('.btn-branding');
+const navItems = document.querySelectorAll('nav-items');
+
+//set initial state of Menu
+let showMenu = false;
