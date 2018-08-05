eb0706365fdd336f031640ff623afafac2a0d7d1
viclotana <viclotana@gmail.com>
2018-08-05 19:43:36 +0100

    opps

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,3 +7,16 @@ const navItems = document.querySelectorAll('nav-items');
 
 //set initial state of Menu
 let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menuBtn.classList.add('show');
+        menuBtn.classList.add('show');
+        menuBtn.classList.add('show');
+    } else {
+
+    }
+}
